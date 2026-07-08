699d7bc102ed34e580343227e5aef29d510badac
SpiritDevPC <spiritcoder@proton.me>
2026-07-07 21:52:37 -0400

    added the first method, BruteForce

---
+++ BruteForce.py
@@ -0,0 +1,30 @@
+import itertools
+import time
+MAXLENGTH = 25
+
+def BruteForce(password):
+
+    Alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+{}:"|<>?-=[]\;,./'
+    startTime = time.time()
+    CharLength = 1
+    counter = 1
+    found = False
+
+    while CharLength <= MAXLENGTH and found == False:
+        attempts = itertools.product(Alphabet,repeat=CharLength)
+        for attempt in attempts:
+            attempt = str(attempt)
+            attempt = attempt.replace(" ","")
+            attempt = attempt.replace("(","")
+            attempt = attempt.replace(")","")
+            attempt = attempt.replace(",","")
+            attempt = attempt.replace("'","")
+            counter += 1
+            if attempt == password:
+                end = time.time()
+                timetaken = end - startTime
+                attemptsPerSecond = counter / timetaken
+                print(f"Your password was: {attempt}. It was found in {timetaken} seconds, {counter} attempts, at a rate of {attemptsPerSecond} attempts per second.")
+                found = True
+
+        CharLength += 1
