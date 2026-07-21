7f2a5130c44eb993a53e7892dcbcb424ed11e09a
SpiritDevPC <spiritcoder@proton.me>
2026-07-21 18:03:49 -0400

    Fixed GIT-LFS utilization

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:05da5eeaf5606e52689b318112697d163f8624a32df6189b5d21f2204b91139b
-size 733
+import time
+
+wordlist_path = "rockyou.txt"
+
+def Dictionary(target):
+
+    found = False
+    counter = 0
+    startTime = time.time()
+    while found == False:
+        with open(wordlist_path, 'r', encoding='latin-1') as file:
+            for word in file:
+                counter += 1
+                word = word.strip()
+                if word == target:
+                    end = time.time()
+                    timetaken = end - startTime
+                    attemptsPerSecond = counter / timetaken
+                    print(f"Your password was: {word}. It was found in {timetaken} seconds, {counter} attempts, at a rate of {attemptsPerSecond} attempts per second.")
+                    found = True
+                    return
+
+    
